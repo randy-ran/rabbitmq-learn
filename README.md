e5a72944e585aa3422b1fa03f8cf623e968e5282
seyfer <seyferseed@mail.ru>
2015-11-23 23:38:49 +0700

    first commit, completed 1 and 2 tutorials

---
+++ README.md
@@ -0,0 +1,4 @@
+This is just my learning process of rabbitMQ tutorials
+
+[Official](http://www.rabbitmq.com/tutorials/)
+[Official github](https://github.com/rabbitmq/rabbitmq-tutorials)
---
+++ php/2.WorkQueues/worker.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seyfer
+ * Date: 11/23/15
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel    = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$callback = function ($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+    sleep(substr_count($msg->body, '.'));
+    echo " [x] Done", "\n";
+
+    /** @var AMQPChannel $channel */
+    $channel = $msg->delivery_info['channel'];
+    $channel->basic_ack($msg->delivery_info['delivery_tag']);
+};
+
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+while (count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
